049418de3fcdc3ea558a796a3007760229a8d141
Andrey2528 <andreys20.2016@ukr.net>
2025-05-08 00:03:28 +0300

    rewrite jsx to tsx

---
+++ src/ThemeContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect, ReactNode } from 'react';
+import React from 'react';
+import '@/styles/index.scss';
+
+interface IThemeProviderProps {
+    children: ReactNode;
+}
+
+type Theme = 'light' | 'dark';
+
+interface IThemeContextType {
+    theme: Theme;
+    toggleTheme: (newTheme: Theme) => void;
+}
+
+// Створюємо контекст з початковим значенням null
+export const ThemeContext = createContext<IThemeContextType | null>(null);
+
+export const ThemeProvider = ({ children }: IThemeProviderProps) => {
+    const savedTheme = (localStorage.getItem('theme') as Theme) || 'light';
+    const [theme, setTheme] = useState<Theme>(savedTheme);
+
+    useEffect(() => {
+        document.body.className = theme;
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    const toggleTheme = (newTheme: Theme) => {
+        setTheme(newTheme);
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};

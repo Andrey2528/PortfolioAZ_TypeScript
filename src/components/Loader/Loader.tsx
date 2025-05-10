b8f537b720bb0f790570520069526c05eae94a5f
Andrey2528 <andreys20.2016@ukr.net>
2025-05-11 01:53:21 +0300

    add loader

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ThemeContext } from '@/ThemeContext'; // заміни шлях, якщо потрібно
+import '@/styles/index.scss';
+import imgDark from '@/assets/logo_spiner_dark.jpg';
+import imgLight from '@/assets/logo_spiner_light.jpg';
+
+const Loader = () => {
+    const themeContext = useContext(ThemeContext);
+    const theme = themeContext?.theme || 'light';
+
+    const imageSrc = theme === 'dark' ? imgDark : imgLight;
+
+    return (
+        <div className={`loader ${theme}`}>
+            <img src={imageSrc} alt="Loading..." className="loader__image" />
+        </div>
+    );
+};
+
+export default Loader;

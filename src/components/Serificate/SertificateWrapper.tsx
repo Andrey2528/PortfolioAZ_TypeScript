04eb451ff63fd2a0366754fb0915c8139bbd2778
Andrey2528 <andreys20.2016@ukr.net>
2025-05-11 22:00:25 +0300

    Add loader to fetch data

--- src/components/Serificate/SertificateWrapper.tsx
+++ src/components/Serificate/SertificateWrapper.tsx
@@ -7,6 +7,7 @@ import Modal from '@/shared/components/Modal/Modal';
 import modalConfig from '@/shared/components/Modal/modalConfig';
 import { getUniqueCompanies } from '@/components/Portfolio/filter';
 import { fetchSertificateCards } from '@/api/connectDB/databasefetch';
+import Loader from '../Loader/Loader';
 
 const SertificateWrapper = () => {
     const { t } = useTranslation();
@@ -16,11 +17,14 @@ const SertificateWrapper = () => {
         null,
     );
     const [selectedCompany, setSelectedCompany] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const getSertificates = async () => {
+            setIsLoading(true);
             const data = await fetchSertificateCards();
             setSertificates(data);
+            setIsLoading(false);
         };
         getSertificates();
     }, []);
@@ -53,54 +57,64 @@ const SertificateWrapper = () => {
 
     return (
         <>
-            <div className="container">
-                <div className="filter__container">
-                    <div className="filter__group">
-                        <label htmlFor="roleFilter" className="filter__label">
-                            {t('filter.company')}
-                        </label>
-                        <select
-                            id="roleFilter"
-                            value={selectedCompany}
-                            onChange={handleCompanyChange}
-                            className="filter__select"
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div className="container">
+                    <div className="filter__container">
+                        <div className="filter__group">
+                            <label
+                                htmlFor="roleFilter"
+                                className="filter__label"
+                            >
+                                {t('filter.company')}
+                            </label>
+                            <select
+                                id="roleFilter"
+                                value={selectedCompany}
+                                onChange={handleCompanyChange}
+                                className="filter__select"
+                            >
+                                <option value="">{t('filter.all')}</option>
+                                {uniqueCompany.map((company) => (
+                                    <option key={company} value={company}>
+                                        {company}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <button
+                            onClick={resetFilters}
+                            className="filter__reset"
                         >
-                            <option value="">{t('filter.all')}</option>
-                            {uniqueCompany.map((company) => (
-                                <option key={company} value={company}>
-                                    {company}
-                                </option>
-                            ))}
-                        </select>
+                            {t('filter.reset')}
+                        </button>
                     </div>
-                    <button onClick={resetFilters} className="filter__reset">
-                        {t('filter.reset')}
-                    </button>
-                </div>
-                <div className="card__list">
-                    {filteredAndSortedCertificates.map((card) => (
-                        <Card<IPortfolioCardFull>
-                            key={card.id}
-                            card={card}
-                            openModal={openModal}
+                    <div className="card__list">
+                        {filteredAndSortedCertificates.map((card) => (
+                            <Card<IPortfolioCardFull>
+                                key={card.id}
+                                card={card}
+                                openModal={openModal}
+                                titleKey="title"
+                                subTitleKey="subTitle"
+                                imgKey="img"
+                                idKey="id"
+                            />
+                        ))}
+                    </div>
+                    {selectedCard && isModalOpen && (
+                        <Modal<IPortfolioCardFull>
+                            card={selectedCard}
+                            onClose={closeModal}
+                            config={modalConfig}
                             titleKey="title"
                             subTitleKey="subTitle"
                             imgKey="img"
-                            idKey="id"
                         />
-                    ))}
+                    )}
                 </div>
-                {selectedCard && isModalOpen && (
-                    <Modal<IPortfolioCardFull>
-                        card={selectedCard}
-                        onClose={closeModal}
-                        config={modalConfig}
-                        titleKey="title"
-                        subTitleKey="subTitle"
-                        imgKey="img"
-                    />
-                )}
-            </div>
+            )}
         </>
     );
 };

ad5b07a8e3b4c20f6344069a2e2c38ad0011c673
Andrey2528 <andreys20.2016@ukr.net>
2025-05-13 21:08:42 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import compression from 'vite-plugin-compression';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), compression(), visualizer()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src/'),
@@ -15,6 +17,13 @@ export default defineConfig({
             output: {
                 manualChunks: {
                     react: ['react', 'react-dom', 'react-router-dom'],
+                    motion: ['framer-motion'],
+                    firebase: ['firebase/app', 'firebase/firestore'],
+                },
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'react-router-dom': 'ReactRouterDOM',
                 },
             },
         },

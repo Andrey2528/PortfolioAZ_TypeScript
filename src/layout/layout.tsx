28e70172489ea984b6354039b2a84ca21593c5f7
Andrey2528 <andreys20.2016@ukr.net>
2025-05-13 19:01:46 +0300

    remove loader

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,7 +3,6 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { useState, useEffect, useContext } from 'react';
 import '@/styles/index.scss';
 import { ThemeContext } from '@/ThemeContext';
-import Loader from '@/components/Loader/Loader';
 
 const Layout = () => {
     const themeContext = useContext(ThemeContext);
@@ -16,21 +15,11 @@ const Layout = () => {
     const { theme, toggleTheme } = themeContext;
 
     const location = useLocation();
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        setIsLoading(true);
-        const timer = setTimeout(() => {
-            setIsLoading(false);
-        }, 500); // Затримка для лоадера (можна налаштувати)
-
-        return () => clearTimeout(timer); // Очищаємо таймер при переході
-    }, [location]);
 
     return (
         <main className={`Layout ${theme}-theme`}>
             <Navbar setTheme={toggleTheme} />
-            {isLoading && <Loader />} {/* Покажемо лоадер під час переходу */}
+
             <section className="Layout__section">
                 <Outlet />
             </section>
